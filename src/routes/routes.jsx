6304e2fd4145d89ebd56e1481ed1cea8080b254b
pare0028 <arthur.parent@etudidant.univ-reims.fr>
2024-03-01 20:41:27 +0100

    Ajout de la route /

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,7 +1,12 @@
-import { Switch } from "wouter";
+import { Route, Switch } from "wouter";
+import FamilleAnimalList from "../views/FamilleAnimalList.jsx";
 
 function Router() {
-  return <Switch />;
+  return (
+    <Switch>
+      <Route path="/" component={FamilleAnimalList}/>
+    </Switch>
+  );
 }
 
 export default Router;
