4b73e4e2bc3a460d16b72c6d2ae7766adb3f5ba3
pare0028 <arthur.parent@etudidant.univ-reims.fr>
2024-03-01 18:24:58 +0100

    Ajout de la fonction fetchAllFamilleAnimals qui retourne la liste des familles d'animaux

--- src/services/api/dataFetch.js
+++ src/services/api/dataFetch.js
@@ -1,1 +1,7 @@
 export const BASE_URL = "http://127.0.0.1:8000/api";
+
+export function fetchAllFamilleAnimals() {
+  return fetch(`${BASE_URL}/famille_animals`).then((response) =>
+    response.json(),
+  );
+}
