19d388523dc09a119ceace01c1fd07b41802f279
pare0028 <arthur.parent@etudidant.univ-reims.fr>
2024-03-02 15:33:45 +0100

    Ajout de la route /familles/id qui affichera le détail de la famille associé à l'id

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,12 +1,16 @@
 import { Route, Switch } from "wouter";
 import React from "react";
 import FamilleAnimalList from "../views/FamilleAnimalList.jsx";
+import FamilleAnimalDetails from "../views/FamilleAnimalDetails.jsx";
 
 function Router() {
   return (
     <Switch>
       <Route path="/familles">
         <FamilleAnimalList />
+      </Route>
+      <Route path="/familles/:id">
+        {(params) => <FamilleAnimalDetails id={params.id} />}
       </Route>
     </Switch>
   );
