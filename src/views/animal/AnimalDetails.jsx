35240f576ca3f73e634a6e7ad94e6817a6299943
pare0028 <arthur.parent@etudidant.univ-reims.fr>
2024-03-03 11:33:38 +0100

    Ajout de la récupération des donnée et du retour de la vue AnimalDetails

--- src/views/animal/AnimalDetails.jsx
+++ src/views/animal/AnimalDetails.jsx
@@ -1,6 +1,17 @@
 import PropTypes from "prop-types";
-import React from "react";
-function AnimalDetails({ id, ...props }) {}
+import React, { useEffect, useState } from "react";
+import { fetchAnimalDetails } from "../../services/api/dataFetch.js";
+import AnimalDetailComponent from "../../components/animal/AnimalDetailComponent.jsx";
+
+function AnimalDetails({ id, ...props }) {
+  const [animal, setAnimal] = useState({});
+  useEffect(() => {
+    fetchAnimalDetails(id).then((data) => {
+      setAnimal(data);
+    });
+  }, []);
+  return <AnimalDetailComponent data={animal}/>
+}
 
 export default AnimalDetails;
 
