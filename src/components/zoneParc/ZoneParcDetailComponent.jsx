94efb6a646d8c5ae809ced7a6b0dd9774b44023c
pare0028 <arthur.parent@etudidant.univ-reims.fr>
2024-03-03 21:25:54 +0100

    Ajout du composant ZoneParcDetail qui affichera les détails d'une zone du parc

---
+++ src/components/zoneParc/ZoneParcDetailComponent.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import PropTypes from "prop-types";
+import ZoneParcDetail from "../../views/zoneParc/ZoneParcDetail.jsx";
+
+function ZoneParcDetailComponent({ data = {}, ...props }) {}
+
+export default ZoneParcDetailComponent;
+
+ZoneParcDetail.PropTypes = {
+  data: PropTypes.array,
+};
