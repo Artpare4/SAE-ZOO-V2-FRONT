49388a42fce0a4735d8a55909ff8b7a03cdd1690
pare0028 <arthur.parent@etudidant.univ-reims.fr>
2024-03-03 21:24:11 +0100

    Création de la vue ZoneParcDetail

---
+++ src/views/zoneParc/ZoneParcDetail.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function ZoneParcDetail({ id, ...props }) {}
+
+export default ZoneParcDetail;
+
+ZoneParcDetail.PropTypes = {
+  id: PropTypes.number,
+};
