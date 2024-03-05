fc509f47a44e65c2e9983af98af280b7ff50cbb8
pare0028 <arthur.parent@etudidant.univ-reims.fr>
2024-03-05 22:05:31 +0100

    Ajout de la vue EventDetail qui affiche les détails d'un event

---
+++ src/views/event/EventDetails.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function EventDetails({ id, ...props }) {}
+
+export default EventDetails;
+
+EventDetails.PropTypes = {
+  id: PropTypes.number,
+};
