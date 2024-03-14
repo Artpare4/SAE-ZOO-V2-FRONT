a7b856f57da22802d57541063188081a72fd0ca6
pare0028 <arthur.parent@etudidant.univ-reims.fr>
2024-03-14 22:32:14 +0100

    Ajout du système de pagination sur la page de liste des events

--- src/views/event/EventList.jsx
+++ src/views/event/EventList.jsx
@@ -1,19 +1,27 @@
 import React, { useEffect, useState } from "react";
 import { fetchAllEvent } from "../../services/api/dataFetch.js";
 import CardList from "../../components/CardList.jsx";
+import paginationFromHydraView from "../../services/transformers/paginationFromHydraView.js";
+import Pagination from "../../components/Pagination.jsx";
 
 function EventList() {
+  const [page, setPage] = useState(1);
   const [events, setEvents] = useState([]);
+  const [pagination, setPagination] = useState([]);
+  const urlParam = new URLSearchParams();
   useEffect(() => {
-    fetchAllEvent().then((value) => {
+    urlParam.append("page", page);
+    fetchAllEvent(urlParam).then((value) => {
       setEvents(value["hydra:member"]);
+      setPagination(paginationFromHydraView(value["hydra:view"]));
     });
-  }, []);
+  }, [page]);
   return (
     <>
       <h1 className="text-center text-8xl lg:text-6xl mt-36 lg:mt-24 mb-5 text-secondary-800">
         Liste des évenements
       </h1>
+      <Pagination setPage={setPage} pagination={pagination} />
       <CardList type="events" data={events} />
     </>
   );
