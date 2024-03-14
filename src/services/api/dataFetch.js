fe61683b7fd4e6a4b3ae85444f1e4be65717c105
pare0028 <arthur.parent@etudidant.univ-reims.fr>
2024-03-14 22:11:35 +0100

    Ajout du paramètre page sur la fonction de récupération de la liste des familles d'animaux

--- src/services/api/dataFetch.js
+++ src/services/api/dataFetch.js
@@ -1,9 +1,9 @@
 export const BASE_URL = "http://127.0.0.1:8000/api";
 
-export function fetchAllFamilleAnimals() {
-  return fetch(`${BASE_URL}/famille_animals`).then((response) =>
-    response.json(),
-  );
+export function fetchAllFamilleAnimals(urlParams = URLSearchParams) {
+  return fetch(
+    `${BASE_URL}/famille_animals?page=${urlParams.get("page")}`,
+  ).then((response) => response.json());
 }
 
 export function ImageElmt(elmt) {
