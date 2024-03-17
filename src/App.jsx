bf60eaa6e0808c4d4436e4a2b28c30fcffc845e8
pare0028 <arthur.parent@etudiant.univ-reims.fr>
2024-03-17 22:11:44 +0100

    Création du composant header

--- src/App.jsx
+++ src/App.jsx
@@ -1,65 +1,13 @@
 import "./App.css";
 import Router from "./routes/routes.jsx";
 import Provider from "./contexts/user/Provider.jsx";
+import Header from "./views/Header.jsx";
 
 function App() {
   return (
     <Provider>
       <div className="App mt-24">
-        <header
-          id="header"
-          className="fixed flex justify-evenly z-10 w-full text-text-50 top-0"
-        >
-          <div className="bg-secondary-800/50 w-full absolute h-24 lg:h-12 z-[-1] bg-clip-padding  backdrop-filter backdrop-blur-sm border-b border-secondary-50 border-s" />
-
-          <div className="btn-container flex justify-between gap-6">
-            <a
-              href="/"
-              className="mt-3 p-4 bg-secondary-800/50  text-text-50 rounded-2xl bg-clip-padding  backdrop-filter backdrop-blur-sm border border-secondary-50 border-s"
-            >
-              Découvrir le parc
-            </a>
-            <a
-              href="/events"
-              className="mt-3 p-4 bg-secondary-800/50  text-text-50 rounded-2xl bg-clip-padding  backdrop-filter backdrop-blur-sm border border-secondary-50 border-s"
-            >
-              Évènements
-            </a>
-            <a
-              href="/familles"
-              className="mt-3 p-4 bg-secondary-800/50  text-text-50 rounded-2xl bg-clip-padding  backdrop-filter backdrop-blur-sm border border-secondary-50 border-s"
-            >
-              Nos animaux
-            </a>
-          </div>
-          <a href="/" className="logo h-20 lg:h-10 pt-1">
-            <img
-              className="h-20 lg:h-10 pt-1"
-              src="src/assets/LogoZoo.png"
-              alt="Logo du Zoo Parc de Laval"
-            />
-          </a>
-          <div className="btn-container flex justify-between gap-6">
-            <a
-              href="/zones"
-              className="mt-3 p-4 bg-secondary-800/50  text-text-50 rounded-2xl bg-clip-padding  backdrop-filter backdrop-blur-sm border border-secondary-50 border-s"
-            >
-              Nos espaces
-            </a>
-            <a
-              href="/login"
-              className="mt-3 p-4 bg-secondary-800/50  text-text-50 rounded-2xl bg-clip-padding  backdrop-filter backdrop-blur-sm border border-secondary-50 border-s"
-            >
-              Votre compte
-            </a>
-            <a
-              href="/reservation"
-              className="mt-3 p-4 bg-primary-500/50 text-text-50 rounded-2xl bg-clip-padding  backdrop-filter backdrop-blur-sm border border-secondary-50 border-s"
-            >
-              Réservez vos billets
-            </a>
-          </div>
-        </header>
+        <Header />
         <Router />
         <footer className="bg-secondary-800 ">
           <div className="w-[90%] grid grid-cols-1 lg:grid-cols-5 m-auto py-3 gap-5">
---
+++ src/views/Header.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header
+      id="header"
+      className="fixed flex justify-evenly z-10 w-full text-text-50 top-0"
+    >
+      <div className="bg-secondary-800/50 w-full absolute h-24 lg:h-12 z-[-1] bg-clip-padding  backdrop-filter backdrop-blur-sm border-b border-secondary-50 border-s" />
+
+      <div className="btn-container flex justify-between gap-6">
+        <a
+          href="/"
+          className="mt-3 p-4 bg-secondary-800/50  text-text-50 rounded-2xl bg-clip-padding  backdrop-filter backdrop-blur-sm border border-secondary-50 border-s"
+        >
+          Découvrir le parc
+        </a>
+        <a
+          href="/events"
+          className="mt-3 p-4 bg-secondary-800/50  text-text-50 rounded-2xl bg-clip-padding  backdrop-filter backdrop-blur-sm border border-secondary-50 border-s"
+        >
+          Évènements
+        </a>
+        <a
+          href="/familles"
+          className="mt-3 p-4 bg-secondary-800/50  text-text-50 rounded-2xl bg-clip-padding  backdrop-filter backdrop-blur-sm border border-secondary-50 border-s"
+        >
+          Nos animaux
+        </a>
+      </div>
+      <a href="/" className="logo h-20 lg:h-10 pt-1">
+        <img
+          className="h-20 lg:h-10 pt-1"
+          src="src/assets/LogoZoo.png"
+          alt="Logo du Zoo Parc de Laval"
+        />
+      </a>
+      <div className="btn-container flex justify-between gap-6">
+        <a
+          href="/zones"
+          className="mt-3 p-4 bg-secondary-800/50  text-text-50 rounded-2xl bg-clip-padding  backdrop-filter backdrop-blur-sm border border-secondary-50 border-s"
+        >
+          Nos espaces
+        </a>
+        <a
+          href="/login"
+          className="mt-3 p-4 bg-secondary-800/50  text-text-50 rounded-2xl bg-clip-padding  backdrop-filter backdrop-blur-sm border border-secondary-50 border-s"
+        >
+          Votre compte
+        </a>
+        <a
+          href="/reservation"
+          className="mt-3 p-4 bg-primary-500/50 text-text-50 rounded-2xl bg-clip-padding  backdrop-filter backdrop-blur-sm border border-secondary-50 border-s"
+        >
+          Réservez vos billets
+        </a>
+      </div>
+    </header>
+  );
+}
+export default Header;
