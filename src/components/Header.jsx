b9349d2742ecfff6a37af612e6b457749d9aef54
pare0028 <arthur.parent@etudiant.univ-reims.fr>
2024-03-18 21:53:38 +0100

    Modification de la source de l'image du header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -41,7 +41,7 @@ function Header() {
       <a href="/" className="logo h-20 lg:h-10 pt-1">
         <img
           className="h-20 lg:h-10 pt-1"
-          src="../assets/LogoZoo.png"
+          src="src/assets/LogoZoo.png"
           alt="Logo du Zoo Parc de Laval"
         />
       </a>
