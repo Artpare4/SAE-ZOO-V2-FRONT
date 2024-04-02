73d04ea0b14c5441ead94101ba7a381859f09c49
Romain Daunat <romain.daunat@etudiant.univ-reims.fr>
2024-04-02 17:13:08 +0200

    Création du composant AdminCard

--- src/components/admin/AdminCard.jsx
+++ src/components/admin/AdminCard.jsx
@@ -1,3 +1,16 @@
 import React from "react";
-import PropTypes from "prop-types";
+import { Link } from "wouter";
+import { CrudUrl } from "../../services/api/dataFetch.js";
 
+function AdminCard() {
+  return (
+    <>
+      <p className="text-4xl lg:text-2xl px-2 text-center">
+        Compte administrateur
+      </p>
+      <Link href={CrudUrl}>Accédez au back-office</Link>
+    </>
+  );
+}
+
+export default AdminCard;
