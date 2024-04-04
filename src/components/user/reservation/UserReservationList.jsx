95ea5223ccb835ff933d15950a44b1935cad6ab9
leco0107 <erwan.lecomte@etudiant.univ-reims.fr>
2024-04-04 11:05:48 +0200

    feat(reservation): Ajout des liens vers l'interface de création d'une réservation

--- src/components/user/reservation/UserReservationList.jsx
+++ src/components/user/reservation/UserReservationList.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import PropTypes from "prop-types";
 import ReservationCard from "./ReservationCard.jsx";
+import {Link} from "wouter";
+import {reservationUrl} from "../../../services/api/dataFetch.js";
 
 function UserReservationList({ data = {}, ...props }) {
   return (
@@ -9,6 +11,12 @@ function UserReservationList({ data = {}, ...props }) {
         <h1 className="text-center text-8xl lg:text-6xl mt-6 lg:mt-24 mb-2 text-secondary-800">
           Vos Réservations
         </h1>
+        <a
+          className="text-center bg-primary-500  mr-2 rounded-2xl p-2 text-4xl lg:text-2xl"
+          href={reservationUrl()}
+        >
+          Réservez votre billet
+        </a>
         {data.map((values) => (
           <ReservationCard data={values} />
         ))}
