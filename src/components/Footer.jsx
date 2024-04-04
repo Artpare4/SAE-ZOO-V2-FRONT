af2d6d2384108362092d9435a054cee552179c48
Romain MONNEY <Romain.monney@etudiant.univ-reims.fr>
2024-04-04 18:37:44 +0200

    suppression de mon Compte

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -33,8 +33,7 @@ function Footer() {
           <a className="text-background-400 text-4xl lg:text-lg" href={'/events'}>
             Programme
           </a>{" "}
-          <br />
-          <a className="text-background-400 text-4xl lg:text-lg" href={'/user'}>Mon compte</a>
+
         </div>
         <div className="lg:col-start-4">
           <h2 className="text-background-200 text-6xl lg:text-2xl mb-3 border-b-2 border-background-200 block">
